b0ac0c46baed18096b87520b35dfcb7dd0532eb8
moinulmithu <moinulmithu@hotmail.com>
2015-02-12 18:32:08 +0600

    Home Controller added

--- DoctorsOnline/Migrations/Configuration.cs
+++ DoctorsOnline/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace DoctorsOnline.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(DoctorsOnline.Models.DoctorsOnlineContext context)
